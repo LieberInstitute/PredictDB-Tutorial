fe83a05ce954a62ae89ea6b845209f93dc98f374
aseyedia <arta.seyedian@gmail.com>
2021-08-03 16:50:18 -0400

    setting paths for computing weights

--- code/gtex_tiss_chrom_training.R
+++ code/gtex_tiss_chrom_training.R
@@ -7,12 +7,13 @@ chrom <- argv[1]
 #tiss <- argv[1]
 #chrom <- argv[2]
 
+# set paths here
 snp_annot_file <- "/dcl01/lieber/ajaffe/lab/goesHyde_mdd_rnaseq/predixcan_pipeline/processed-data/02_prep_inputs/snp_annot/snp_annot.chr" %&% chrom %&% ".txt"
 gene_annot_file <- "/dcl01/lieber/ajaffe/lab/goesHyde_mdd_rnaseq/predixcan_pipeline/processed-data/02_prep_inputs/gencode.v25.annotationGRCh38_PARSED.gtf"
 genotype_file <- "/dcl01/lieber/ajaffe/lab/goesHyde_mdd_rnaseq/predixcan_pipeline/processed-data/02_prep_inputs/split_geno/split_snp_geno.chr" %&% chrom %&% ".txt"
 expression_file <- "/dcl01/lieber/ajaffe/lab/goesHyde_mdd_rnaseq/predixcan_pipeline/processed-data/02_prep_inputs"
 covariates_file <- "/dcl01/lieber/ajaffe/lab/goesHyde_mdd_rnaseq/predixcan_pipeline/processed-data/01_get_inv_quantile_norm/goesHyde_mdd_rnaseq_Amygdala.combined_covariates.txt"
-prefix <- "Model_training"
+prefix <- "goesHyde_mdd_Model_training"
 
 main(snp_annot_file, gene_annot_file, genotype_file, expression_file, covariates_file, as.numeric(chrom), prefix, null_testing=FALSE)
 
