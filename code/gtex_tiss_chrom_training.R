6beeb6aca985341133df622b15eb9f142493de96
aseyedia <arta.seyedian@gmail.com>
2021-08-03 12:14:00 -0400

    specifying goesHyde_mdd paths

--- code/gtex_tiss_chrom_training.R
+++ code/gtex_tiss_chrom_training.R
@@ -1,4 +1,4 @@
-source("../code/gtex_v7_nested_cv_elnet.R")
+source("gtex_v7_nested_cv_elnet.R")
 "%&%" <- function(a,b) paste(a,b, sep='')
 
 argv <- commandArgs(trailingOnly = TRUE)
@@ -7,11 +7,11 @@ chrom <- argv[1]
 #tiss <- argv[1]
 #chrom <- argv[2]
 
-snp_annot_file <- "../output/snp_annot.chr" %&% chrom %&% ".txt"
-gene_annot_file <- "../output/gene_annot.parsed.txt"
-genotype_file <- "../output/genotype.chr" %&% chrom %&% ".txt"
-expression_file <- "../output/transformed_expression.txt"
-covariates_file <- "../output/covariates.txt"
+snp_annot_file <- "/dcl01/lieber/ajaffe/lab/goesHyde_mdd_rnaseq/predixcan_pipeline/processed-data/02_prep_inputs/snp_annot/snp_annot.chr" %&% chrom %&% ".txt"
+gene_annot_file <- "/dcl01/lieber/ajaffe/lab/goesHyde_mdd_rnaseq/predixcan_pipeline/processed-data/02_prep_inputs/gencode.v25.annotationGRCh38_PARSED.gtf"
+genotype_file <- "/dcl01/lieber/ajaffe/lab/goesHyde_mdd_rnaseq/predixcan_pipeline/processed-data/02_prep_inputs/split_geno/split_snp_geno.chr" %&% chrom %&% ".txt"
+expression_file <- "/dcl01/lieber/ajaffe/lab/goesHyde_mdd_rnaseq/predixcan_pipeline/processed-data/02_prep_inputs"
+covariates_file <- "/dcl01/lieber/ajaffe/lab/goesHyde_mdd_rnaseq/predixcan_pipeline/processed-data/01_get_inv_quantile_norm/goesHyde_mdd_rnaseq_Amygdala.combined_covariates.txt"
 prefix <- "Model_training"
 
 main(snp_annot_file, gene_annot_file, genotype_file, expression_file, covariates_file, as.numeric(chrom), prefix, null_testing=FALSE)
